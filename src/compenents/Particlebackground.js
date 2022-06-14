20f6e9ccf352d54b8ec4653fc7651c514050bee9
Spi051997 <dcodeacedemy97@gmail.com>
2022-06-14 18:05:06 +0530

    Package-lock jason repair

--- src/compenents/Particlebackground.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Particles from "react-tsparticles";
-import Particlconfig from "./config/Particleconfig";
-
-const Particlebackground = () => {
-  return <Particles options={Particlconfig}></Particles>;
-};
-
-export default Particlebackground;
